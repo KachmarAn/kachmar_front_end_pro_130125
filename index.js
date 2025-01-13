fddafa59da61b03cc7d910804fe91081537f747f
KachmarAN <bilux.teplo@gmail.com>
2025-01-13 20:05:15 +0200

    додано js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log ()
+console.log ('hello world');
+console.log ('hello world');
+console.log ('hello world');
+console.log ('hello world');
+console.log ('hello world');
+console.log ('hello world');
+console.log ('hello world');
