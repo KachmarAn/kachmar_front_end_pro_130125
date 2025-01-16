b561e4617f7b3bd0405dda006cc9f69c6ebb5e1c
KachmarAN <bilux.teplo@gmail.com>
2025-01-16 20:55:59 +0200

    commit

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 let hours = prompt("Введіть кількість годин:");
-
-if (!isNaN(hours) && hours > 0) {
- 
-    let seconds = hours * 3600;
-  
-    alert(`У ${hours} годинах міститься ${seconds} секунд.`);
-} else {
-  alert("Будь ласка, введіть коректне число годин.");
-}
+let seconds = hours * 3600;
+alert(`У ${hours} годинах міститься ${seconds} секунд.`);
